2189f69ba1370d2884f269228ee0ee0e43a5d234
Kevin Ward <kevin@control-plane.io>
2021-10-20 16:08:33 +0100

    chore: updated gitignore

--- pkg/rules/defaultNamespace_test.go
+++ pkg/rules/defaultNamespace_test.go
@@ -7,33 +7,42 @@ import (
 
 // -- TODO KGW Write tests
 
-// func Test_AllowedNamespaces(t *testing.T) {
-// 	var data = `
-// ---
-// apiVersion: apps/v1
-// kind: Deployment
-// spec:
-//   template:
-//     spec:
-//       containers:
-//         - name: c1
-//           securityContext:
-//             allowPrivilegeEscalation: true
-//         - name: c2
-//           securityContext:
-//             allowPrivilegeEscalation: true
-//         - name: c3
-//           securityContext:
-//             allowPrivilegeEscalation: true
-// `
+func Test_NonDefaultNamespace(t *testing.T) {
+	var data = `
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: example
+`
 
-// 	json, err := yaml.YAMLToJSON([]byte(data))
-// 	if err != nil {
-// 		t.Fatal(err.Error())
-// 	}
+	json, err := yaml.YAMLToJSON([]byte(data))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
 
-// 	containers := AllowPrivilegeEscalation(json)
-// 	if containers != 3 {
-// 		t.Errorf("Got %v containers wanted %v", containers, 3)
-// 	}
-// }
+	operator := DefaultNamespace(json)
+	if operator != 0 {
+		t.Errorf("Got %v operator wanted %v", operator, 0)
+	}
+}
+
+func Test_DefaultNamespace(t *testing.T) {
+	var data = `
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: kube-system
+`
+
+	json, err := yaml.YAMLToJSON([]byte(data))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	operator := DefaultNamespace(json)
+	if operator != 1 {
+		t.Errorf("Got %v operator wanted %v", operator, 1)
+	}
+}
