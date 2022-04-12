74f109ad8c96030176ad657f881d76f86c8d690e
Kevin Ward <kevin@control-plane.io>
2022-04-12 09:27:31 +0100

    fix: remove unrequired comments from no seccontext rulesets

--- pkg/rules/noSecurityContext.go
+++ pkg/rules/noSecurityContext.go
@@ -17,14 +17,9 @@ func NoSecurityContext(json []byte) int {
 	jqSecurityContext := gojsonq.New().Reader(bytes.NewReader(json)).
 		From(spec + ".securityContext")
 
-	// fmt.Printf("%v", jqContainers.Count())
-	// fmt.Printf("%v\n", jqContainers.Get())
-	// fmt.Printf("%v", jqSecurityContext.Count())
-	// fmt.Printf("%v\n", jqSecurityContext.Get())
-
 	if jqContainers.Count() == 0 && jqSecurityContext.Count() == 0 {
 		sc++
 	}
 
-	return sc //+ jqSecurityContext.Count()
+	return sc
 }
