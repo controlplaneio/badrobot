e5c8d94f32878f0735c2236000e2ecb27b3e0a52
Henry Mortimer <henry.mortimer@control-plane.io>
2022-05-04 11:10:47 +0100

    fix(admission controller cluster role): match with multiple api groups

--- pkg/rules/admissionControllerClusterRole.go
+++ pkg/rules/admissionControllerClusterRole.go
@@ -24,51 +24,51 @@ func AdmissionControllerClusterRole(json []byte) int {
 		From("rules").
 		Only("verbs")
 
-	if (strings.Contains(fmt.Sprintf("%v", jqAPI), "[admissionregistration.k8s.io")) &&
+	if (strings.Contains(fmt.Sprintf("%v", jqAPI), "admissionregistration.k8s.io")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqResources), "mutatingwebhookconfigurations")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqVerbs), "*")) {
 		rbac++
-	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "[admissionregistration.k8s.io")) &&
+	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "admissionregistration.k8s.io")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqResources), "mutatingwebhookconfigurations")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqVerbs), "create")) {
 		rbac++
-	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "[admissionregistration.k8s.io")) &&
+	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "admissionregistration.k8s.io")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqResources), "mutatingwebhookconfigurations")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqVerbs), "patch")) {
 		rbac++
-	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "[admissionregistration.k8s.io")) &&
+	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "admissionregistration.k8s.io")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqResources), "mutatingwebhookconfigurations")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqVerbs), "update")) {
 		rbac++
-	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "[admissionregistration.k8s.io")) &&
+	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "admissionregistration.k8s.io")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqResources), "mutatingwebhookconfigurations")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqVerbs), "delete")) {
 		rbac++
-	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "[admissionregistration.k8s.io")) &&
+	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "admissionregistration.k8s.io")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqResources), "mutatingwebhookconfigurations")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqVerbs), "deletecollection")) {
 		rbac++
-	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "[admissionregistration.k8s.io")) &&
+	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "admissionregistration.k8s.io")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqResources), "validatingwebhookconfigurations")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqVerbs), "*")) {
 		rbac++
-	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "[admissionregistration.k8s.io")) &&
+	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "admissionregistration.k8s.io")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqResources), "validatingwebhookconfigurations")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqVerbs), "create")) {
 		rbac++
-	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "[admissionregistration.k8s.io")) &&
+	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "admissionregistration.k8s.io")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqResources), "validatingwebhookconfigurations")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqVerbs), "patch")) {
 		rbac++
-	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "[admissionregistration.k8s.io")) &&
+	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "admissionregistration.k8s.io")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqResources), "validatingwebhookconfigurations")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqVerbs), "update")) {
 		rbac++
-	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "[admissionregistration.k8s.io")) &&
+	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "admissionregistration.k8s.io")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqResources), "validatingwebhookconfigurations")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqVerbs), "delete")) {
 		rbac++
-	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "[admissionregistration.k8s.io")) &&
+	} else if (strings.Contains(fmt.Sprintf("%v", jqAPI), "admissionregistration.k8s.io")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqResources), "validatingwebhookconfigurations")) &&
 		(strings.Contains(fmt.Sprintf("%v", jqVerbs), "deletecollection")) {
 		rbac++
