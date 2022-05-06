c3ed2250f814ba1fd94b3635732012eb7827d6cc
Henry Mortimer <henry.mortimer@control-plane.io>
2022-05-06 16:05:34 +0100

    refactor(star all core api cluster role): use kubeapi for parsing

--- pkg/rules/starAllCoreAPIClusterRole.go
+++ pkg/rules/starAllCoreAPIClusterRole.go
@@ -2,45 +2,39 @@
 package rules
 
 import (
-	"bytes"
-	"fmt"
-	"strconv"
-	"strings"
+	"encoding/json"
 
-	"github.com/thedevsaddam/gojsonq/v2"
+	rbacv1 "k8s.io/api/rbac/v1"
 )
 
-func StarAllCoreAPIClusterRole(json []byte) int {
+func StarAllCoreAPIClusterRole(input []byte) int {
 	rbac := 0
 
-	jqRules := gojsonq.New().Reader(bytes.NewReader(json)).
-		From("rules")
+	clusterRole := &rbacv1.ClusterRole{}
+	err := json.Unmarshal(input, clusterRole)
+	if err != nil {
+		return 0
+	}
 
-	numElementsStr := fmt.Sprintf("%v", jqRules.Count())
-	numElementsVar, _ := strconv.Atoi(numElementsStr)
-
-	for i := 1; i <= numElementsVar; i++ {
-		apiGroups := fmt.Sprintf("%v", jqRules.Nth(i).(map[string]interface{})["apiGroups"])
-		resources := fmt.Sprintf("%v", jqRules.Nth(i).(map[string]interface{})["resources"])
-		verbs := fmt.Sprintf("%v", jqRules.Nth(i).(map[string]interface{})["verbs"])
-
-		if strings.Contains(fmt.Sprintf("%v", apiGroups), "[]") &&
-			strings.Contains(fmt.Sprintf("%v", resources), "*") &&
-			strings.Contains(fmt.Sprintf("%v", verbs), "*") {
+	for _, rule := range clusterRole.Rules {
+		if contains("", rule.APIGroups) &&
+			contains("*", rule.Resources) &&
+			contains("*", rule.Verbs) {
 			rbac++
-		} else if strings.Contains(fmt.Sprintf("%v", apiGroups), "[]") &&
-			strings.Contains(fmt.Sprintf("%v", resources), "*") &&
-			strings.Contains(fmt.Sprintf("%v", verbs), "get") &&
-			strings.Contains(fmt.Sprintf("%v", verbs), "create") &&
-			strings.Contains(fmt.Sprintf("%v", verbs), "update") &&
-			strings.Contains(fmt.Sprintf("%v", verbs), "list") &&
-			strings.Contains(fmt.Sprintf("%v", verbs), "patch") &&
-			strings.Contains(fmt.Sprintf("%v", verbs), "watch") &&
-			strings.Contains(fmt.Sprintf("%v", verbs), "delete") &&
-			strings.Contains(fmt.Sprintf("%v", verbs), "deletecollection") {
+		} else if contains("", rule.APIGroups) &&
+			contains("*", rule.Resources) &&
+			containsAll([]string{
+				"get",
+				"create",
+				"update",
+				"list",
+				"patch",
+				"watch",
+				"delete",
+				"deletecollection",
+			}, rule.Verbs) {
 			rbac++
 		}
-
 	}
 
 	return rbac
