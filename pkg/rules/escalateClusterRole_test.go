77bcc3b9c0656cbf24a9e744c0be49bc0080b676
Henry Mortimer <henry.mortimer@control-plane.io>
2022-05-09 16:37:40 +0100

    refactor(escalate cluster role): use kubeapi for parsing

--- pkg/rules/escalateClusterRole_test.go
+++ pkg/rules/escalateClusterRole_test.go
@@ -33,6 +33,38 @@ rules:
 	}
 }
 
+func Test_Escalate_Permissions_Multiple_Rules(t *testing.T) {
+	var data = `
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: example-operator
+rules:
+- apiGroups:
+  - rbac.authorization.k8s.io
+  resources:
+  - clusterroles
+  verbs:
+  - escalate
+- apiGroups:
+  - ""
+  resources:
+  - pods
+  verbs:
+  - "*"
+`
+
+	json, err := yaml.YAMLToJSON([]byte(data))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	rbac := EscalateClusterRole(json)
+	if rbac != 1 {
+		t.Errorf("Got %v permissions wanted %v", rbac, 1)
+	}
+}
 func Test_Incorrect_Escalate_Permissions(t *testing.T) {
 	var data = `
 ---
